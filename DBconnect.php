b8913bd86ea539abd77fc61d0d67858ccf1fcd44
Mieke Campher <147103548+mieke-campher@users.noreply.github.com>
2025-02-12 13:11:39 +0200

    Update DBconnect.php

--- DBconnect.php
+++ DBconnect.php
@@ -1,3 +1,3 @@
 <?php
-$pdo=new PDO('mysql:host=sql206.infinityfree.com;port=3306;dbname=if0_38299513_RetroTone_DB', 'if0_38299513', '73023Huisies');
+$pdo=new PDO('mysql:host=localhost;port=3306;dbname=RetroTone_DB', 'root', '');
 ?>
